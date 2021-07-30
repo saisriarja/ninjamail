7c676f9d4c359bff3f1c239ddf31ff5b284f62be
sai sri <saisriarja@gamil.com>
2021-07-30 21:50:58 +0530

    services-page

--- js/main.js
+++ js/main.js
@@ -54,4 +54,28 @@ $(document).ready(function(){
         });
         return $flag;
     });
+    $('.hid').hide();
+    $('.click').click(function(){
+        $('.hid').toggle();
+    });
+    $('.hid-2').hide();
+    $('.click-2').click(function(){
+        $('.hid-2').toggle();
+    });
+    $('.hid-3').hide();
+    $('.click-3').click(function(){
+        $('.hid-3').toggle();
+    });
+    $('.hid-4').hide();
+    $('.click-4').click(function(){
+        $('.hid-4').toggle();
+    });
+    $('.hid-5').hide();
+    $('.click-5').click(function(){
+        $('.hid-5').toggle();
+    });
+    $('.hid-6').hide();
+    $('.click-6').click(function(){
+        $('.hid-6').toggle();
+    });
 });
